8f2d70c01c948a57984f3bb4fd1fa4a14519ddae
haritha <harithapnair2205@gmail.com>
2020-05-08 19:05:01 -0430

    javascript file

---
+++ calculator.js
@@ -0,0 +1,28 @@
+let add = (num1,num2) =>
+{
+    let num3=num1+num2
+    return num3
+}
+let sub = (num1,num2) =>
+{
+    let num3=num1-num2
+    return num3
+}
+let mul = (num1,num2) =>
+{
+    let num3=num1*num2
+    return num3
+}
+let div = (num1,num2) =>
+{
+    let num3=num1/num2
+    return num3
+}
+let addition = add(7,8)
+let subtraction = sub(7,8)
+let multiplication = mul(7,8)
+let division = div(7,8)
+console.log('Addition of 7 and 8 is :',addition)
+console.log('Subtraction of 7 and 8 is :',subtraction)
+console.log('Multiplication of 7 and 8 is :',multiplication)
+console.log('Division of 7 and 8 is :',division)
